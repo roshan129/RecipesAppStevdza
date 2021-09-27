8dc1ab4c8791ca221077e5a993c0f301f456ce94
microsoft <roshanadke2015@gmail.com>
2021-09-27 18:47:39 +0530

    commit: 27-09-2021 06:46 pm

--- app/src/main/java/com/adivid/recipesappstevdza/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/adivid/recipesappstevdza/ui/fragments/recipes/RecipesFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.adivid.recipesappstevdza.R
 import com.adivid.recipesappstevdza.viewmodels.MainViewModel
@@ -24,6 +25,8 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args by navArgs<RecipesFragmentArgs>()
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
@@ -66,7 +69,7 @@ class RecipesFragment : Fragment() {
     private fun readDatabase() {
         lifecycleScope.launch {
             mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
-                if (database.isNotEmpty()) {
+                if (database.isNotEmpty() && !args.backFromBottomSheet) {
                     Log.d("TAG", "readDatabase: called")
                     mAdapter.setData(database[0].foodRecipe)
                     hideShimmerEffect()
--- app/src/main/java/com/adivid/recipesappstevdza/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/adivid/recipesappstevdza/viewmodels/RecipesViewModel.kt
@@ -1,7 +1,12 @@
 package com.adivid.recipesappstevdza.viewmodels
 
 import android.app.Application
+import android.util.Log
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.adivid.recipesappstevdza.data.DataStoreRepository
+import com.adivid.recipesappstevdza.data.MealAndDietType
 import com.adivid.recipesappstevdza.util.Constants.Companion.API_KEY
 import com.adivid.recipesappstevdza.util.Constants.Companion.DEFAULT_DIET_TYPE
 import com.adivid.recipesappstevdza.util.Constants.Companion.DEFAULT_MEAL_TYPE
@@ -12,17 +17,42 @@ import com.adivid.recipesappstevdza.util.Constants.Companion.QUERY_DIET
 import com.adivid.recipesappstevdza.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.adivid.recipesappstevdza.util.Constants.Companion.QUERY_NUMBER
 import com.adivid.recipesappstevdza.util.Constants.Companion.QUERY_TYPE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+class RecipesViewModel @ViewModelInject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
 
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
 
     fun applyQueries(): HashMap<String, String> {
-        val     queries: HashMap<String, String> = HashMap()
+        val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+
+        }
+
 
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
